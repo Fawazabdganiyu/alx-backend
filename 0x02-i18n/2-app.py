04f67e59d9505ec1c98c67e7d2c9527d3f5c7687
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-10 03:43:42 +0100

    Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Flask application definition
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configure available languages and default settings
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get the best match supported language from user browser
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Welcome page
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
